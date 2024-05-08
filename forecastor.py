4d971ba8a994f6ce8cfd4932948ac6333bbc1794
Barry Huang <zxc5356061@gmail.com>
2024-05-08 22:50:12 +0200

    add return_coef

--- forecastor.py
+++ forecastor.py
@@ -36,22 +36,45 @@ def persistence_Naive_MAPE(raw_df:pd.DataFrame, code:str, lag:int, test_periods)
         return round(mape*100,3)
 
     
-def train_model_AR(raw_df:pd.DataFrame, code:str, lag:int, test_periods, alpha_bottom = 0.01):
+    
+def train_model_AR(raw_df:pd.DataFrame, code:str, lag:int, test_periods, alpha_bottom = 0.01, return_coef = False):
     """
     Train Lasso models based on individual RM code with only autoregression features and given test periods
     
-    X_train headers
+    ## X_train headers
     - lag = 1: ['AR_1', 'AR_2', 'AR_3', 'AR_4', 'AR_5', 'AR_6', 'AR_7', 'AR_8', 'AR_9', 'AR_10', 'AR_11', 'AR_12']
     - lag = 3: ['AR_3', 'AR_4', 'AR_5', 'AR_6', 'AR_7', 'AR_8', 'AR_9', 'AR_10', 'AR_11', 'AR_12']
     - lag = 6: ['AR_6', 'AR_7', 'AR_8', 'AR_9', 'AR_10', 'AR_11', 'AR_12']
     
-    Model parameters:
+    ## Model parameters:
     - param_grid = {'alpha': np.linspace(alpha_bottom, 1, 3000)}
     - random_search = RandomizedSearchCV(estimator=lasso,
                                          param_distributions=param_grid,
                                          n_iter=300,
                                          cv=5,
                                          random_state=42)
+                                         
+    ## If you want to print out coef of all models, return_coef == True:                                         
+    coef_list = list()
+    for code in RM_codes:
+
+        for lag in lags:
+            test_df = pd.DataFrame()
+            count = 0
+
+            for period in test_periods:
+                count += 1
+                mape, coef_sum = train_model_AR(feature_df,code,lag,period,alpha_bottom, True)
+                temp = pd.DataFrame.from_dict(coef_sum, 
+                                              orient='index',
+                                              columns=[f"{code}_lag-{lag}_test-{count}"])
+                coef_list.append(temp)
+
+
+    merged_df = pd.concat(coef_list, axis=1)
+    result = pd.DataFrame.transpose(merged_df)
+    print(result)
+   
     """
     assert not len(raw_df) == 0, "df is empty!"
     assert not len(code) == 0, "RM_codes are missed."
@@ -114,25 +137,57 @@ def train_model_AR(raw_df:pd.DataFrame, code:str, lag:int, test_periods, alpha_b
     y_pred_test_inverse = scaler_y.inverse_transform(y_pred_test.reshape(-1,1))
     mape = mean_absolute_percentage_error(y_test,y_pred_test_inverse)
     assert mape > 0, "mape is negative!"
-    return mape*100
-
-
-def train_model_all_features(raw_df:pd.DataFrame, code:str, lag:int, test_periods, alpha_bottom = 0.01):
+    
+    ## To return coef of each models if necessary
+    if return_coef == True:
+        coef_sum = dict()
+        assert random_search.n_features_in_ == len(X_test.columns), "n_features !=len(X_test.columns)"
+        for feature, coefficient in zip(X_test.columns, random_search.best_estimator_.coef_):
+            coef_sum[feature] = round(coefficient, 5)
+        return mape*100, coef_sum
+    else:
+        return mape*100
+    
+    
+    
+def train_model_all_features(raw_df:pd.DataFrame, code:str, lag:int, test_periods, alpha_bottom = 0.01, return_coef = False):
     """
     Train Lasso models based on individual RM code with autoregression features and external price drivers and given test periods
     
-    X_train headers
+    ## X_train headers
     - lag = 1: ['Electricity_1', 'Electricity_2', ... 'Electricity_12', 'AR_1', 'AR_2' ... 'AR_12']
     - lag = 3: ['Electricity_3', 'Electricity_4', ... 'Electricity_12', 'AR_3', 'AR_4' ... 'AR_12']
     - lag = 6: ['Electricity_6', 'Electricity_7', ... 'Electricity_12', 'AR_6', 'AR_7' ... 'AR_12']
     
-    Model parameters:
+    ## Model parameters:
     - param_grid = {'alpha': np.linspace(alpha_bottom, 1, 3000)}
     - random_search = RandomizedSearchCV(estimator=lasso,
                                          param_distributions=param_grid,
                                          n_iter=300,
                                          cv=5,
                                          random_state=42)
+                                         
+    ## If you want to print out coef of all models, return_coef == True:                                         
+    coef_list = list()
+    for code in RM_codes:
+
+        for lag in lags:
+            test_df = pd.DataFrame()
+            count = 0
+
+            for period in test_periods:
+                count += 1
+                mape, coef_sum = train_model_all_features(feature_df,code,lag,period,alpha_bottom, True)
+                temp = pd.DataFrame.from_dict(coef_sum, 
+                                              orient='index',
+                                              columns=[f"{code}_lag-{lag}_test-{count}"])
+                coef_list.append(temp)
+
+
+    merged_df = pd.concat(coef_list, axis=1)
+    result = pd.DataFrame.transpose(merged_df)
+    print(result)
+   
     """
     assert not len(raw_df) == 0, "df is empty!"
     assert not len(code) == 0, "RM_codes are missed."
@@ -195,5 +250,16 @@ def train_model_all_features(raw_df:pd.DataFrame, code:str, lag:int, test_period
     y_pred_test_inverse = scaler_y.inverse_transform(y_pred_test.reshape(-1,1))
     mape = mean_absolute_percentage_error(y_test,y_pred_test_inverse)
     assert mape > 0, "mape is negative!"
-    return mape*100
+
+    ## To return coef of each models if necessary
+    if return_coef == True:
+        coef_sum = dict()
+        assert random_search.n_features_in_ == len(X_test.columns), "n_features !=len(X_test.columns)"
+        for feature, coefficient in zip(X_test.columns, random_search.best_estimator_.coef_):
+            coef_sum[feature] = round(coefficient, 5)
+        return mape*100, coef_sum
+    else:
+        return mape*100
+    
+
 
