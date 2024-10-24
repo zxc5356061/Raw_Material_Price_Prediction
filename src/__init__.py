66a05e1a45dd29552afe4eed9226db98fda30a16
p.huang <p.huang@house-of-communication.com>
2024-10-24 18:35:44 +0200

    Added: virtual environment Python 3.10.12

---
+++ src/__init__.py
@@ -0,0 +1,43 @@
+from src.preprocessor import (
+    get_Fred_data,
+    clean_elec_csv,
+    clean_pred_price_evo_csv,
+    impute_pred_price_evo_csv,
+    get_dummies_and_average_price,
+    exclude_imputed_data_from_y,
+    generate_features,
+    get_interaction_terms
+)
+
+from src.forecastor import (
+    persistence_Naive_MAPE,
+    train_model_AR,
+    train_model_all_features
+)
+
+from src.visualiser import (
+    draw_graph
+)
+
+from src.naiveforecastor import (
+    naive_forest
+)
+
+# The __all__ variable in __init__.py is used to define which symbols (functions, classes, etc.)
+# should be accessible when using from module import *.
+# It’s a good practice to explicitly define the functions you want to expose for public use.
+__all__ = [
+    get_Fred_data,
+    clean_elec_csv,
+    clean_pred_price_evo_csv,
+    impute_pred_price_evo_csv,
+    get_dummies_and_average_price,
+    exclude_imputed_data_from_y,
+    generate_features,
+    get_interaction_terms,
+    persistence_Naive_MAPE,
+    train_model_AR,
+    train_model_all_features,
+    draw_graph,
+    naive_forest
+]
